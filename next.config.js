dcabe92d373cf89b6e4842bf41c03bea6364b5c8
MOON <muneon96@naver.com>
2024-05-21 17:47:11 +0900

    chore: 외부 이미지 경로 설정

--- next.config.js
+++ next.config.js
@@ -11,6 +11,38 @@ const nextConfig = {
         protocol: "https",
         hostname: "codeit-images.codeit.com",
       },
+      {
+        protocol: "https",
+        hostname: "codeit-frontend.codeit.com",
+      },
+      {
+        protocol: "https",
+        hostname: "reactjs.org",
+      },
+      {
+        protocol: "https",
+        hostname: "assets.vercel.com",
+      },
+      {
+        protocol: "https",
+        hostname: "tanstack.com",
+      },
+      {
+        protocol: "https",
+        hostname: "storybook.js.org",
+      },
+      {
+        protocol: "https",
+        hostname: "testing-library.com",
+      },
+      {
+        protocol: "https",
+        hostname: "static.cdninstagram.com",
+      },
+      {
+        protocol: "https",
+        hostname: "s.pstatic.net",
+      },
     ],
   },
 };
