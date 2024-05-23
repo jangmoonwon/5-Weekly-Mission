056144850ac41e181543846b7fb0fd5d56aed7a2
MOON <muneon96@naver.com>
2024-05-23 18:37:29 +0900

    Refactor: 외부 이미지 경로 수정

--- next.config.js
+++ next.config.js
@@ -2,48 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "codeit-front.s3.ap-northeast-2.amazonaws.com",
-      },
-      {
-        protocol: "https",
-        hostname: "codeit-images.codeit.com",
-      },
-      {
-        protocol: "https",
-        hostname: "codeit-frontend.codeit.com",
-      },
-      {
-        protocol: "https",
-        hostname: "reactjs.org",
-      },
-      {
-        protocol: "https",
-        hostname: "assets.vercel.com",
-      },
-      {
-        protocol: "https",
-        hostname: "tanstack.com",
-      },
-      {
-        protocol: "https",
-        hostname: "storybook.js.org",
-      },
-      {
-        protocol: "https",
-        hostname: "testing-library.com",
-      },
-      {
-        protocol: "https",
-        hostname: "static.cdninstagram.com",
-      },
-      {
-        protocol: "https",
-        hostname: "s.pstatic.net",
-      },
-    ],
+    remotePatterns: [{ protocol: "https", hostname: "**" }],
   },
 };
 
