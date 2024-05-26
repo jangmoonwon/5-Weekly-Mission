9ca7205120a0eb5a92a220b3b432016b92ce7ccd
MOON <muneon96@naver.com>
2024-05-26 22:55:15 +0900

    refactor: folder, shared 페이지 수정

--- pages/folder/index.tsx
+++ pages/folder/index.tsx
@@ -1,51 +1,46 @@
 import styles from "./Folder.module.scss";
 import classNames from "classnames/bind";
-import AddLinkBar from "@/components/add-link-bar/AddLinkBar";
+import AddLinkBar from "@/components/folder/add-link-bar/AddLinkBar";
 import Layout from "@/components/layout/layout/Layout";
-import FolderLayout from "@/components/layout/page-layout/folder-layout/FolderLayout";
-import ToolBar from "@/components/tool-bar/ToolBar";
-import CardList from "@/components/card-list/CardList";
+import ToolBar from "@/components/folder/tool-bar/ToolBar";
+import CardList from "@/components/ui/card-list/CardList";
 import { useLinks } from "../api/useLinks";
 import formatDate from "@/lib/formatDate";
 import { getElapsedTime } from "@/lib/getElapsedTime";
-import Card from "@/components/card/Card";
+import Card from "@/components/ui/card/Card";
+import SearchBar from "@/components/shared/search-bar/SearchBar";
 
 const cx = classNames.bind(styles);
 
-interface CardProps {
-  id: number;
-  url: string;
-  image_source: string;
-  alt: string;
-  elapsedTime: string;
-  description: string;
-  created_at: string;
-}
-
 export default function Folder() {
-  const { data } = useLinks();
+  const { data, isLoading, isError } = useLinks();
+
+  if (isLoading) return <div>loading</div>;
+  if (isError) return <div>error</div>;
+  if (!data) return <div>data없음</div>;
 
   return (
     <Layout>
-     <div className={cx("container")}>
-      <AddLinkBar />
-      <div className={cx("item")}>
-        <ToolBar />
-        <CardList>
-          {data?.data.map((item: CardProps) => (
-            <Card
-              key={item.id}
-              href={item.url}
-              imageSource={item.image_source}
-              alt={"card"}
-              elapsedTime={getElapsedTime(item.created_at)}
-              description={item.description}
-              createdAt={formatDate(item.created_at)}
-            />
-          ))}
-        </CardList>
+      <div className={cx("container")}>
+        <AddLinkBar />
+        <div className={cx("item")}>
+          <SearchBar />
+          <ToolBar />
+          <CardList>
+            {data.map((item) => (
+              <Card
+                key={item.id}
+                href={item.url}
+                imageSource={item.image_source}
+                alt={"card"}
+                elapsedTime={getElapsedTime(item.created_at)}
+                description={item.description}
+                createdAt={formatDate(item.created_at)}
+              />
+            ))}
+          </CardList>
+        </div>
       </div>
-    </div>
     </Layout>
   );
 }
--- pages/shared/index.tsx
+++ pages/shared/index.tsx
@@ -1,49 +1,43 @@
 import Layout from "@/components/layout/layout/Layout";
-import { useFolder } from "../api/useFolder";
-import FolderInfo from "@/components/folder-info/FolderInfo";
-import SearchBar from "@/components/search-bar/SearchBar";
-import CardList from "@/components/card-list/CardList";
-import Card from "@/components/card/Card";
+import FolderInfo from "@/components/shared/folder-info/FolderInfo";
+import SearchBar from "@/components/shared/search-bar/SearchBar";
+import CardList from "@/components/ui/card-list/CardList";
+import Card from "@/components/ui/card/Card";
 import formatDate from "@/lib/formatDate";
 import { getElapsedTime } from "@/lib/getElapsedTime";
 import styles from "./Shared.module.scss";
 import classNames from "classnames/bind";
+import { useFolder } from "@/pages/hooks-test/useFolder";
 
 const cx = classNames.bind(styles);
 
-interface CardProps {
-  id: number;
-  url: string;
-  imageSource: string;
-  alt: string;
-  elapsedTime: string;
-  description: string;
-  createdAt: string;
-}
-
 export default function Shared() {
-  const { data } = useFolder();
+  const { data, isLoading, isError } = useFolder();
+
+  if (isLoading) return <div>loading</div>;
+  if (isError) return <div>error</div>;
+  if (!data) return <div>data가 없음</div>;
 
   return (
     <Layout isSticky={false}>
       <div className={cx("container")}>
         <FolderInfo
-          folderName={data?.folder.name}
-          ownerName={data?.folder.owner.name}
-          profileImage={data?.folder.owner.profileImageSource}
+          folderName={data.name}
+          ownerName={data.owner.name}
+          profileImage={data.owner.profileImageSource}
         />
         <div className={cx("item")}>
           <SearchBar />
           <CardList>
-            {data?.folder.links.map((item: CardProps) => (
+            {data.links.map((link) => (
               <Card
-                key={item.id}
-                href={item.url}
-                imageSource={item.imageSource}
+                key={link.id}
+                href={link.url}
+                imageSource={link.imageSource}
                 alt={"card"}
-                elapsedTime={getElapsedTime(item.createdAt)}
-                description={item.description}
-                createdAt={formatDate(item.createdAt)}
+                elapsedTime={getElapsedTime(link.createdAt)}
+                description={link.description}
+                createdAt={formatDate(link.createdAt)}
               />
             ))}
           </CardList>
