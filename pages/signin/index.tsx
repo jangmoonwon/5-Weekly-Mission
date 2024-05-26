c765fa44fb7c61915e60fe45a67baa963635f5c3
MOON <muneon96@naver.com>
2024-05-26 22:57:14 +0900

    refactor: signin, signup페이지 수정

--- pages/signin/index.tsx
+++ pages/signin/index.tsx
@@ -1,18 +1,32 @@
 import { LoginLayout } from "@/components/layout/page-layout/auth-layout/LoginLayout";
 import { AuthHeader } from "@/components/auth/ui-auth-header/AuthHeader";
+import { AuthHeaderLogo } from "@/components/auth/ui-auth-header-logo/AuthHeaderLogo";
+import { AuthHeaderDescAction } from "@/components/auth/ui-auth-header-desc-action/AuthHeaderDescAction";
 import { SigninForm } from "@/components/auth/signin-form/SigninForm";
 import { SocialBox } from "@/components/auth/ui-auth-social-box/SocialBox";
+import { AuthHeaderDescription } from "@/components/auth/ui-auth-header-description/AuthHeaderDescription";
+import { AuthHeaderContent } from "@/components/auth/ui-auth-header-content/AuthHeaderContent";
 
 export default function Signin() {
   return (
     <LoginLayout>
-      <AuthHeader
-        href="/signup"
-        message="회원이 아니신가요?"
-        linkName="회원 가입하기"
-      />
+      <A.Header>
+        <A.HeaderLogo />
+        <A.HeaderContent>
+          <A.HeaderDescription>회원이 아니신가요?</A.HeaderDescription>
+          <A.HeaderDescAction href="/signup">회원 가입하기</A.HeaderDescAction>
+        </A.HeaderContent>
+      </A.Header>
       <SigninForm />
       <SocialBox text="소셜 로그인" />
     </LoginLayout>
   );
 }
+
+export const A = {
+  Header: AuthHeader,
+  HeaderLogo: AuthHeaderLogo,
+  HeaderContent: AuthHeaderContent,
+  HeaderDescription: AuthHeaderDescription,
+  HeaderDescAction: AuthHeaderDescAction,
+};
--- pages/signup/index.tsx
+++ pages/signup/index.tsx
@@ -1,18 +1,32 @@
 import { LoginLayout } from "@/components/layout/page-layout/auth-layout/LoginLayout";
 import { AuthHeader } from "@/components/auth/ui-auth-header/AuthHeader";
+import { AuthHeaderLogo } from "@/components/auth/ui-auth-header-logo/AuthHeaderLogo";
+import { AuthHeaderDescAction } from "@/components/auth/ui-auth-header-desc-action/AuthHeaderDescAction";
 import { SignupForm } from "@/components/auth/signup-form/SignupForm";
 import { SocialBox } from "@/components/auth/ui-auth-social-box/SocialBox";
+import { AuthHeaderDescription } from "@/components/auth/ui-auth-header-description/AuthHeaderDescription";
+import { AuthHeaderContent } from "@/components/auth/ui-auth-header-content/AuthHeaderContent";
 
 export default function Signup() {
   return (
     <LoginLayout>
-      <AuthHeader
-        href="/signin"
-        message="이미 회원이신가요?"
-        linkName="로그인 하기"
-      />
+      <A.Header>
+        <A.HeaderLogo />
+        <A.HeaderContent>
+          <A.HeaderDescription>이미 회원이신가요?</A.HeaderDescription>
+          <A.HeaderDescAction href="/signin">로그인 하기</A.HeaderDescAction>
+        </A.HeaderContent>
+      </A.Header>
       <SignupForm />
       <SocialBox text="다른 방식으로 가입하기" />
     </LoginLayout>
   );
 }
+
+export const A = {
+  Header: AuthHeader,
+  HeaderLogo: AuthHeaderLogo,
+  HeaderContent: AuthHeaderContent,
+  HeaderDescription: AuthHeaderDescription,
+  HeaderDescAction: AuthHeaderDescAction,
+};
